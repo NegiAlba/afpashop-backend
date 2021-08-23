e97d6d765065d2dbc70ca8175c47b27a4489d1d7
Axel Gromat <axel.gromat@gmail.com>
2021-08-23 17:54:13 +0200

    Changed SSL process

--- config/database.js
+++ config/database.js
@@ -16,9 +16,11 @@ module.exports = ({ env }) => {
             database: config.database,
             username: config.user,
             password: config.password,
+            ssl: {
+              rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+            },
           },
           options: {
-            ssl: false,
           },
         },
       },
